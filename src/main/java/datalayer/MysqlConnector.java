b14c7f1e5abb61a47c8025c0ce57d919702336f4
Dominique <niehau@gmail.com>
2019-01-18 13:53:07 +0100

    connection with DB can be made
    hardcoded login has been moved to property file

--- src/main/java/datalayer/MysqlConnector.java
+++ src/main/java/datalayer/MysqlConnector.java
@@ -2,13 +2,15 @@ package datalayer;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ResourceBundle;
 
 public class MysqlConnector {
 
+    private static ResourceBundle rb = ResourceBundle.getBundle("connector");
     private static MysqlConnector instance;
-    private String databaseName = "sys";
-    private String user = "root";
-    private String password = "Password123";
+    private String databaseName = rb.getString("databaseName");
+    private String user = rb.getString("user");
+    private String password = rb.getString("password");
 
     public static MysqlConnector getInstance() {
         if(instance == null) {
@@ -21,7 +23,7 @@ public class MysqlConnector {
     public Connection connect() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/" + databaseName, user, password);
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + databaseName +"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", user, password);
             return connection;
         } catch(Exception e) {
             System.out.println(e);
