946f6c9f1f64e3ba22f58c840dd6db3c5ca31b1f
Dominique <niehau@gmail.com>
2019-01-20 02:40:40 +0100

    added CRUD for watched percentage

--- src/main/java/datalayer/MovieDAO.java
+++ src/main/java/datalayer/MovieDAO.java
@@ -2,6 +2,7 @@ package datalayer;
 
 import datalayerInterface.IMovie;
 import model.Movie;
+import model.Profile;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -103,5 +104,78 @@ public class MovieDAO implements IMovie {
         return m;
     }
 
+    @Override @SuppressWarnings("Duplicates")
+    public void addWatchedPercentage(Movie m, Profile p, int percentage) {
+        Connection con = null;
+        String query = "INSERT INTO watched VALUES('profile_id' = ?, 'movie_id' = ?, 'watched_percentage' = ?)";
 
+        try {
+            con = MysqlConnector.getInstance().connect();
+            PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1, p.getProfileID());
+            st.setInt(2, m.getMovieID());
+            st.setInt(3, percentage);
+
+            st.executeUpdate();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updateWatchedPercentage(Movie m, Profile p, int percentage) {
+        Connection con = null;
+        String query = "UPDATE watched SET watched_percentage = ? WHERE profile_id = ? AND movie_id = ?";
+
+        try {
+            con = MysqlConnector.getInstance().connect();
+            PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1, percentage);
+            st.setInt(2, p.getProfileID());
+            st.setInt(3, m.getMovieID());
+
+            st.executeUpdate();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override @SuppressWarnings("Duplicates")
+    public void deleteWatchedPercentage(Movie m, Profile p) {
+        Connection con = null;
+        String query = "DELETE FROM watched WHERE profile_id = ? AND movie_id = ?";
+
+        try {
+            con = MysqlConnector.getInstance().connect();
+            PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1, p.getProfileID());
+            st.setInt(2, m.getMovieID());
+
+            st.executeUpdate();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override @SuppressWarnings("Duplicates")
+    public int getWatchedPercentage(Movie m, Profile p) {
+        Connection con = null;
+        int watchedPercentage = 0;
+        String query = "SELECT watched.watched_percentage FROM watched WHERE profile_id = ? AND movie_id = ?";
+
+        try {
+            con = MysqlConnector.getInstance().connect();
+            PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1, p.getProfileID());
+            st.setInt(2, m.getMovieID());
+            ResultSet rs = st.executeQuery();
+
+            while(rs.next()) {
+                watchedPercentage = rs.getInt("watched_percentage");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return watchedPercentage;
+    }
 }
--- src/main/java/datalayerInterface/IMovie.java
+++ src/main/java/datalayerInterface/IMovie.java
@@ -1,6 +1,7 @@
 package datalayerInterface;
 
 import model.Movie;
+import model.Profile;
 
 import java.util.HashSet;
 
@@ -11,4 +12,12 @@ public interface IMovie {
     HashSet<Movie> getAllMovies();
 
     Movie getLongestMovieUnderSixteen();
+
+    void addWatchedPercentage(Movie m, Profile p, int percentage);
+
+    void updateWatchedPercentage(Movie m, Profile p, int percentage);
+
+    void deleteWatchedPercentage(Movie m, Profile p);
+
+    int getWatchedPercentage(Movie m, Profile p);
 }
