69b8dcc723d3445bd9d8c7b646acc7836c239c5f
Dominique <niehau@gmail.com>
2019-01-20 04:43:59 +0100

    added CRUD for episode watch percentage

--- src/main/java/datalayer/EpisodeDAO.java
+++ src/main/java/datalayer/EpisodeDAO.java
@@ -2,6 +2,7 @@ package datalayer;
 
 import datalayerInterface.IEpisode;
 import model.Episode;
+import model.Profile;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -73,5 +74,104 @@ public class EpisodeDAO implements IEpisode {
             ex.printStackTrace();
         }
         return ep;
+    }
+
+    @Override
+    public int getAverageWatchTime(Episode e) {
+        Connection con = null;
+        int averageWatchPercentage = 0;
+        String query = "SELECT AVG(watched.watched_percentage) AS average_watch_percentage FROM watched WHERE episode_id = ? ";
+
+        try{
+            con = MysqlConnector.getInstance().connect();
+            PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1, e.getEpisodeID());
+            ResultSet rs = st.executeQuery();
+
+            while(rs.next()){
+                averageWatchPercentage = Math.round(rs.getInt("average_watch_percentage"));
+            }
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return averageWatchPercentage;
+    }
+
+    @Override
+    public void addWatchedPercentage(Episode e, Profile p, int percentage) {
+        Connection con = null;
+        String query = "INSERT INTO watched VALUES('profile_id' = ?, 'episode_id' = ?, 'watched_percentage' = ?)";
+
+        try {
+            con = MysqlConnector.getInstance().connect();
+            PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1, p.getProfileID());
+            st.setInt(2, e.getEpisodeID());
+            st.setInt(3, percentage);
+
+            st.executeUpdate();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override @SuppressWarnings("Duplicates")
+    public int getWatchedPercentage(Episode e, Profile p) {
+        Connection con = null;
+        int watchedPercentage = 0;
+        String query = "SELECT watched.watched_percentage FROM watched WHERE episode_id = ? AND profile_id = ?";
+
+        try {
+            con = MysqlConnector.getInstance().connect();
+            PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1, e.getEpisodeID());
+            st.setInt(2, p.getProfileID());
+            ResultSet rs = st.executeQuery();
+
+            while(rs.next()) {
+                watchedPercentage = rs.getInt("watched_percentage");
+            }
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return watchedPercentage;
+    }
+
+    @Override
+    public void updateWatchedPercentage(Episode e, Profile p, int percentage) {
+        Connection con = null;
+        String query = "UPDATE watched SET watched_percentage = ? WHERE episode_id = ? AND profile_id = ?";
+
+        try {
+            con = MysqlConnector.getInstance().connect();
+            PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1, percentage);
+            st.setInt(2, e.getEpisodeID());
+            st.setInt(3, p.getProfileID());
+
+            st.executeUpdate();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override @SuppressWarnings("Duplicates")
+    public void deleteWatchedPercentage(Episode e, Profile p) {
+
+        Connection con = null;
+        String query = "DELETE FROM watched WHERE episode_id = ? AND profile_id = ?";
+
+        try {
+            con = MysqlConnector.getInstance().connect();
+            PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1, e.getEpisodeID());
+            st.setInt(2, p.getProfileID());
+
+            st.executeUpdate();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/datalayerInterface/IEpisode.java
+++ src/main/java/datalayerInterface/IEpisode.java
@@ -1,6 +1,8 @@
 package datalayerInterface;
 
 import model.Episode;
+import model.Profile;
+
 import java.util.List;
 
 public interface IEpisode {
@@ -8,4 +10,14 @@ public interface IEpisode {
     List<Episode> getAllEpisodes();
 
     Episode getEpisodeByName(String title);
+
+    int getAverageWatchTime(Episode e);
+
+    void addWatchedPercentage(Episode e, Profile p, int percentage);
+
+    int getWatchedPercentage(Episode e, Profile p);
+
+    void updateWatchedPercentage(Episode e, Profile p, int percentage);
+
+    void deleteWatchedPercentage(Episode e, Profile p);
 }
